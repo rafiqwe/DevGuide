7093b3b91cfc5a00e9e8721d1d92086536bede12
rafiqwe <rmlmanik502@gmail.com>
2025-07-13 22:41:33 +0600

    create login route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/user.controller')
+const userController = require("../controllers/user.controller");
 const { body } = require("express-validator");
 router.get("/", (req, res) => {
   res.send("Hello  user route from DevGuide Backend");
@@ -18,6 +18,15 @@ router.post(
       .withMessage("Password must be at least 6 characters long"),
   ],
   userController.registerUser
+);
+
+router.post("/login", [
+  body("email").isEmail().withMessage("Invalid Email"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+],
+userController.loginUser
 );
 
 module.exports = router;
