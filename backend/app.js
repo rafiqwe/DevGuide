10f813b3f55c9a9052e630d164970451475c26b5
rafiqwe <rmlmanik502@gmail.com>
2025-07-13 20:04:50 +0600

    Add all routes like  user, video, bookmark in app.ja

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,21 @@
-// backend/app.js
 require("dotenv").config();
 const express = require("express");
+const videoRoutes = require("./routes/video.route");
+const userRoute = require("./routes/user.route");
+const bookMarkRoute = require("./routes/bookMark.route");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Routes
+
 app.get("/", (req, res) => {
   res.send("Hello from DevGuide backend!");
 });
+
+app.use("/user", userRoute);
+app.use("/api/videos", videoRoutes);
+app.use("/api/bookmark", bookMarkRoute);
 
 module.exports = app;
