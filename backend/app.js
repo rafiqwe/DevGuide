7d41a50621985e1652a08efcaf5fa0de4d6643cd
rafiqwe <rmlmanik502@gmail.com>
2025-07-15 21:53:42 +0600

    Install multer

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
 const videoRoutes = require("./routes/video.route");
@@ -5,6 +6,7 @@ const userRoute = require("./routes/user.route");
 const bookMarkRoute = require("./routes/bookMark.route");
 
 const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
