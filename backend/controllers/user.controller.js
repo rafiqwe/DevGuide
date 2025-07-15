844878a396541174373de7bbe8a1848276580fc1
rafiqwe <rmlmanik502@gmail.com>
2025-07-15 21:49:52 +0600

    create login,logout,profile, change profile page

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -11,7 +11,7 @@ module.exports.registerUser = async (req, res) => {
     const { fullname, email, password } = req.body;
 
     console.log(req.body);
-    
+
     const existingUser = await userModel.findOne({ email });
     if (existingUser) {
       return res.status(409).json({ message: "User already exists" });
@@ -32,5 +32,78 @@ module.exports.registerUser = async (req, res) => {
   } catch (error) {
     console.error("Error registering user:", err);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports.loginUser = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = user.generateAuthToken();
+    res.cookie("token", token, { httpOnly: true });
+
+    user.password = undefined;
+
+    res.status(200).json({ token, user });
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports.getUserProfile = async (req, res) => {
+  res.status(200).json(req.user);
+};
+
+module.exports.logoutUser = async (req, res) => {
+  try {
+    const token =
+      req.cookies?.token || req.headers?.authorization?.split(" ")[1];
+
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logged Out" });
+  } catch (err) {
+    console.error("Logout error:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports.changeProfile = async (req, res) => {
+  const imageBuffer = req.file?.buffer;
+  const user = req.user;
+  if (!imageBuffer) {
+    return res.status(400).json({ message: "Image is required." });
+  }
+
+  try {
+    const updatedUser = await userModel.findOneAndUpdate(
+      { email: user.email },
+      { image: imageBuffer },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    res.status(200).json({ message: "Profile image updated successfully." });
+  } catch (error) {
+    console.error("Error updating profile image:", error);
+    res.status(500).json({ message: "Internal Server Error." });
   }
 };
