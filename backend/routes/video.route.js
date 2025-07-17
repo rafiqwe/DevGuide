fa50e7534c8f1b28ef6fd514ad9c38ccbfca271e
rafiqwe <rmlmanik502@gmail.com>
2025-07-17 23:38:48 +0600

    Create single video route

--- backend/routes/video.route.js
+++ backend/routes/video.route.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { query } = require("express-validator");
-const getVideos = require("../controllers/video.controller");
-
+const { query, param } = require("express-validator");
+const videoController = require("../controllers/video.controller");
 
 router.get(
   "/",
   [
     query("topic")
-      .isEmpty()
+      .isString()
       .isLength({ min: 3 })
-      .withMessage("topic must be 3  characters long"),
+      .withMessage("Topic must be at least 3 characters"),
   ],
-  getVideos
+  videoController.getVideos
 ); // /api/videos?query=react
+
+router.get(
+  "/single/:videoId",
+  [
+    param("videoId")
+      .isString()
+      .isLength({ min: 3 })
+      .withMessage("video Id  must be at least 3 characters"),
+  ],
+  videoController.getSingleVideo
+);
 
 module.exports = router;
