84d8af1700c90e120b180072d50fb763447858e0
rafiqwe <rmlmanik502@gmail.com>
2025-07-17 23:37:43 +0600

    Create video service page

---
+++ backend/services/video.service.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+module.exports.getSingleVideo = async ({ videoId, apiKey }) => {
+  try {
+    const videoRes = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos`,
+      {
+        params: {
+          part: "snippet,statistics",
+          id: videoId,
+          key: apiKey,
+        },
+      }
+    );
+
+    return videoRes;
+  } catch (error) {
+    console.log("video service error:", error);
+  }
+};
