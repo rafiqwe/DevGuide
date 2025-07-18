71c409ae3ce17989976dd78de5c7c79363976d3a
rafiqwe <rmlmanik502@gmail.com>
2025-07-18 23:53:23 +0600

    Create Protected wraper

---
+++ frontend/src/pages/ProtectedWraper.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
+
+const ProtectedWraper = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const token = localStorage.getItem("token");
+  const { setuser, setIsPopupOpen } = useContext(UserDataContext);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+        setIsPopupOpen(true)
+    //   navigate("/login");
+    }
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setuser(response.data);
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (loading) return <div className="text-center mt-10">Loading...</div>;
+
+  return <>{children}</>;
+};
+
+export default ProtectedWraper;
