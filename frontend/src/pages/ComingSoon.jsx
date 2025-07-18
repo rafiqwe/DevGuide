3b455323e7c1a7bdeb43a1e31b5dc7c800569cd5
rafiqwe <rmlmanik502@gmail.com>
2025-07-18 23:52:03 +0600

    Create Coming soon page

---
+++ frontend/src/pages/ComingSoon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ComingSoon = () => {
+  return (
+    <div className="w-full h-full  ">
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="max-w-2xl w-full ">
+          <h1 className="text-4xl font-bold text-center mb-8 text-gray-900 dark:text-white">
+            Coming Soon!
+          </h1>
+          <p className="text-lg text-gray-600 dark:text-gray-300 text-center mb-12">
+            Web page is under construction. We'll be back soon!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
