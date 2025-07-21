54fe66f01988e2a4fcc0a672bc87817220effb74
rafiqwe <rmlmanik502@gmail.com>
2025-07-21 14:52:05 +0600

    Updated bookmark model

--- backend/models/bookmark.model.js
+++ backend/models/bookmark.model.js
@@ -4,7 +4,7 @@ const bookMarkSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "user",
       required: true,
     },
     videoId: {
