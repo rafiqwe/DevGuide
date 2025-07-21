3daede40f0a5f19b330b207a135418ca65751971
rafiqwe <rmlmanik502@gmail.com>
2025-07-21 15:00:21 +0600

    Change somethings

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -1,8 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import { IoMdCloseCircle } from "react-icons/io";
 
-const Popup = ({ setIsPopupOpen }) => {
+const Popup = () => {
   const navigate = useNavigate();
 
   const handleBack = () => {
@@ -21,13 +20,13 @@ const Popup = ({ setIsPopupOpen }) => {
         transition={{ duration: 0.5 }}
         className="bg-white/10 dark:bg-white/5 backdrop-blur-lg rounded-2xl p-8 max-w-xl w-full shadow-2xl border border-white/20 text-center"
       >
-        <h2 className="text-3xl font-bold dark:text-white text-white mb-4">
+        <h2 className="text-3xl font-bold dark:text-white text-gray-700 mb-4">
           🚫 Login Required
         </h2>
         <p className="dark:text-gray-300 text-black mb-6 leading-relaxed">
           You must be logged in to view this page. Just like nurturing a plant
           takes time and care 🌱, access here starts with a simple step —
-          logging in. 
+          logging in.
         </p>
         <div className="flex flex-col sm:flex-row justify-center gap-4 mt-6">
           <button
