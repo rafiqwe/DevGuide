8ad54aea1efb517129a263c1f4e38b25af4f2441
rafiqwe <rmlmanik502@gmail.com>
2025-07-22 19:07:37 +0600

    Created coming soon page

--- frontend/src/pages/ComingSoon.jsx
+++ frontend/src/pages/ComingSoon.jsx
@@ -5,6 +5,9 @@ const ComingSoon = () => {
     <div className="w-full h-full  ">
       <div className="min-h-screen flex items-center justify-center">
         <div className="max-w-2xl w-full ">
+          <div className="flex items-center justify-center mb-5">
+            <img className="w-50 h-50" src="../../public/images/coming-soon.webp"/>
+          </div>
           <h1 className="text-4xl font-bold text-center mb-8 text-gray-900 dark:text-white">
             Coming Soon!
           </h1>
