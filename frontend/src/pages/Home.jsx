92c4e2792335fd55c89558997d94ec4105db56f3
rafiqwe <rmlmanik502@gmail.com>
2025-07-30 12:27:29 +0600

    Fetch data using react query

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,54 @@
 import { useContext, useEffect } from "react";
 import VideoCard from "../components/VideoCard";
 import { SearchContext } from "../context/SearchContext";
-import Popup from "../components/Popup";
-import { usePopup } from "../context/PopupContext";
+import { useQuery } from "@tanstack/react-query";
+import API from "../services/api";
+import { SkeletonCard } from "../components/SkeletonCard";
 
 const Home = () => {
   const { setSearchResults } = useContext(SearchContext);
+
+  const getHomeData = async () => {
+    try {
+      const res = await API.get("/api/home");
+      return res.data;
+    } catch (error) {
+      console.log("Home page fatching error:", error.message);
+      throw new Error({ message: "Home page error" });
+    }
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["playlists"],
+    queryFn: getHomeData,
+  });
 
   useEffect(() => {
     setSearchResults([]);
     localStorage.removeItem("searchResults"); // ✅ Clear only on Home
   }, [setSearchResults]);
 
-  const videos = [...Array(12)].map((_, i) => ({
-    id: "abc123",
-    thumbnail: "https://i.ytimg.com/vi/abc123/hqdefault.jpg",
-    duration: "12:45",
-    title: "How to Build a YouTube Clone in React JS",
-    channelId: "channel123",
-    channelName: "DevGuide Channel",
-    channelAvatar:
-      "https://www.shutterstock.com/image-vector/letter-dev-simple-logo-design-600nw-1784944118.jpg",
-    views: "1.2M views",
-    timeAgo: "2 days ago",
-  }));
+  if (isLoading) {
+    return (
+      <div
+        className="grid gap-8 
+                grid-cols-1 
+                sm:grid-cols-2 
+                md:grid-cols-3 
+                lg:grid-cols-4
+                items-center w-full mx-auto"
+      >
+        {Array.from({ length: 8 }).map((_, i) => (
+          <SkeletonCard key={i} />
+        ))}
+      </div>
+    );
+  }
 
   return (
     <>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 p-4">
-        {videos.map((video, idx) => (
+        {data?.map((video, idx) => (
           <VideoCard key={idx} video={video} />
         ))}
       </div>
