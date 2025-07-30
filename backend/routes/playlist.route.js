2c06e98ae166404f523dd9ce69b6f21d4146ef1f
rafiqwe <rmlmanik502@gmail.com>
2025-07-30 12:54:40 +0600

    Modify playlist route /link route

--- backend/routes/playlist.route.js
+++ backend/routes/playlist.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const playlistController = require("../controllers/playlist.controller");
-const { body } = require("express-validator");
+const { body, query } = require("express-validator");
 
 router.post(
   "/",
@@ -13,6 +13,18 @@ router.post(
   playlistController.addPlaylist
 );
 
-router.get('/', playlistController.getPlaylists)
+router.get("/", playlistController.getPlaylists);
+
+router.get(
+  "/list",
+  [
+    query("playlistId")
+      .isString()
+      .withMessage("playlistId must be a string")
+      .isLength({ min: 3 })
+      .withMessage("playlistId must be at least 3 characters"),
+  ],
+  playlistController.getSinglePlayList
+);
 
 module.exports = router;
