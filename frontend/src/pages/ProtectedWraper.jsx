a895531aaf2ecd0b0d4becf7965d67fce8cb88c4
rafiqwe <rmlmanik502@gmail.com>
2025-08-26 19:48:47 +0600

    Fixed protected wraper

--- frontend/src/pages/ProtectedWraper.jsx
+++ frontend/src/pages/ProtectedWraper.jsx
@@ -3,7 +3,6 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserDataContext } from "../context/UserContext";
 import { usePopup } from "../context/PopupContext";
-import ProfileSkeleton from "./ProfileSkeleton";
 
 const ProtectedWraper = ({ children }) => {
   const { setuser } = useContext(UserDataContext);
@@ -31,15 +30,14 @@ const ProtectedWraper = ({ children }) => {
 
         if (res.status === 200) {
           setuser(res.data);
-        } else {
-          throw new Error("Unauthorized");
         }
       } catch (err) {
+        localStorage.removeItem("token");
         console.error("Auth error:", err.message);
         setIsPopupOpen(true);
         // Optionally redirect:
         // navigate("/login");
-      } 
+      }
     };
 
     fetchProfile();
