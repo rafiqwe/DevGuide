3a848b66e847c0516e8b6cf922068df80f0fe722
rafiqwe <rmlmanik502@gmail.com>
2025-08-26 20:41:53 +0600

    remove vercel analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next"
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
