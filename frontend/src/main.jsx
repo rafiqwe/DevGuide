b59f043c1acd42b08654d3956edff0dfd480ffd4
rafiqwe <rmlmanik502@gmail.com>
2025-09-01 16:22:29 +0600

    Install react helmet for seo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,19 +6,22 @@ import UserContext from "./context/UserContext.jsx";
 import { SearchProvider } from "./context/SearchContext.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PopupProvider } from "./context/PopupContext.jsx";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <PopupProvider>
-        <SearchProvider>
-          <UserContext>
-            <App />
-          </UserContext>
-        </SearchProvider>
-      </PopupProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <PopupProvider>
+          <SearchProvider>
+            <UserContext>
+              <App />
+            </UserContext>
+          </SearchProvider>
+        </PopupProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   </StrictMode>
 );
